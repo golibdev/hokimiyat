f85e6fc8a7781daf364367b26d377ec6daf46842
gnarzulllayev <gnarzulllayev2000@gmail.com>
2023-02-14 18:51:52 +0500

    add project

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
    return (
       <div className='d-flex align-items-center' style={{ position: 'absolute', right: '50px', bottom: '0' }}>
+         <span className='fw-bold'>Powered by</span>
          <img width={100} src="/assets/it-park.png" alt="it-park" />
-         <span className='fw-bold'>Powered by IT Park</span>
       </div>
    )
 }
